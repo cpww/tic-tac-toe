9eb24e6596915135e90724302d1dc280f0545454
shiggiddie <sean.dennison.osu@gmail.com>
2014-11-19 22:15:46 -0600

    implemented human vs computer (computer needs to go center, though)

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ function Vector(x, y) {
     this.x = x;
     this.y = y;
     this.value = null;
-    console.log('Vector Created!');
+    //console.log('Vector Created!');
 };
 
 Vector.prototype.set = function(val) {
     this.value = val;
-    console.log('Vector set to: ' + val);
+    //console.log('Vector set to: ' + val);
 }
 
 /*/////////////////////////////////////
@@ -34,7 +34,8 @@ function Grid() {
     this.vectors = [new Vector(0, 0), new Vector(1, 0), new Vector(2, 0),
                     new Vector(0, 1), new Vector(1, 1), new Vector(2, 1),
                     new Vector(0, 2), new Vector(1, 2), new Vector(2, 2)];
-    console.log('Grid Created!');
+    this.winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    //console.log('Grid Created!');
 };
 
 Grid.prototype.display = function() {
@@ -59,19 +60,18 @@ Grid.prototype.display = function() {
 }
 
 Grid.prototype.setVector = function(index, value) {
-    console.log('Setting Vector: ' + index + ' to: ' + value);
+    //console.log('Setting Vector: ' + index + ' to: ' + value);
     this.vectors[index].set(value);
 }
 
 Grid.prototype.hasWin = function() {
     //var surroundingVectors = this.getSurroundingVectors(index);
 
-    var winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
     var self = this;
 
     // Fuck reduce
-    return winCombos.some(function(element, index, array) {
+    return this.winCombos.some(function(element, index, array) {
 
         var currentCombo = [self.vectors[element[0]].value, self.vectors[element[1]].value, self.vectors[element[2]].value];
 
@@ -90,21 +90,95 @@ function Player(type, value) {
 var prompt = require('sync-prompt').prompt;
 
 function onErr(err) {
-  console.log(err);
+  //console.log(err);
   return 1;
+}
+
+var count_in_array = function(array, val) {
+  return array.filter(function(el) {
+    return el === val;
+  }).length;
 }
 
 Player.prototype.makeMove = function(grid) {
   var invalidMove = true;
-  while (invalidMove) {
-    var index = prompt('Enter a number: ') - 1;
-    if ([0,1,2,3,4,5,6,7,8].indexOf(index) > -1 && grid.vectors[index].value == null) {
-      grid.setVector(index, this.value);
-      invalidMove = false;
+  if (this.type == 'human') {
+    while (invalidMove) {
+      var index = prompt('Enter a number: ') - 1;
+      if ([0,1,2,3,4,5,6,7,8].indexOf(index) > -1 && grid.vectors[index].value == null) {
+        grid.setVector(index, this.value);
+        invalidMove = false;
+      }
+      else {
+        console.log("Please enter a valid number");
+      }
+    }
+  }
+  else {
+    // computer move code goes here
+    var self = this;
+    var enemy;
+    if (self.value == 'O') {
+      enemy = 'X';
     }
     else {
-      console.log("Please enter a valid number");
+      enemy = 'O';
     }
+
+    // The Vector index we'll set on grid's vectors array
+    var index = null;;
+
+    //console.log('computer is attempting to win');
+    // First, try to win...
+    grid.winCombos.some(function(winCombo) {
+      values = [grid.vectors[winCombo[0]].value, grid.vectors[winCombo[1]].value, grid.vectors[winCombo[2]].value]
+      if (count_in_array(values, null) == 1 && count_in_array(values, self.value) == 2) {
+        index = winCombo[values.indexOf(null)];
+        return true;
+      }
+    });
+    //console.log('computer index is now: ' + index);
+    if (index == null) {
+      //console.log('computer is attempting to block');
+      // No win?  Try to block...
+      grid.winCombos.some(function(winCombo) {
+        values = [grid.vectors[winCombo[0]].value, grid.vectors[winCombo[1]].value, grid.vectors[winCombo[2]].value]
+        //console.log('values is: ' + values);
+        //console.log('enemy is: ' + enemy);
+        //console.log('count_in_array null: ' + count_in_array(values, null));
+        //console.log('count_in_array ' + enemy + ' : ' + count_in_array(values, enemy));
+        if (count_in_array(values, null) == 1 && count_in_array(values, enemy) == 2 ) {
+          index = winCombo[values.indexOf(null)];
+          return true;
+        }
+      });
+    }
+    //console.log('computer index is now: ' + index);
+    if (index == null) {
+      // No block?  Try a corner...
+      //console.log('computer is attempting to corner');
+      var corners = [0, 2, 6, 8];
+      var open_corners = corners.filter(function(corner) {
+          return grid.vectors[corner].value == null;
+      });
+      if (open_corners.length) {
+        index = open_corners[Math.floor(Math.random()*open_corners.length)]
+      }
+    }
+    //console.log('computer index is now: ' + index);
+    if (index == null) {
+      // Geez, just try wherever...
+      //console.log('computer is attempting to wherever');
+      var remainers = [1, 3, 4, 5, 7];
+      var open_remainers = remainers.filter(function(remainer) {
+          return grid.vectors[remainer].value == null;
+      });
+      if (open_remainers.length) {
+        index = open_remainers[Math.floor(Math.random()*open_remainers.length)]
+      }
+    }
+    //console.log('computer index is now: ' + index);
+    grid.setVector(index, this.value);
   }
 }
 
@@ -114,7 +188,7 @@ Player.prototype.makeMove = function(grid) {
 /////////////////////////////////////*/
 function Game() {
   this.grid = new Grid();
-  this.players = [new Player('human', 'X'), new Player('human', 'O')]
+  this.players = [new Player('human', 'X'), new Player('computer', 'O')]
   this.turn = 0
   this.currentPlayer = Math.floor(Math.random()*2)
 }
